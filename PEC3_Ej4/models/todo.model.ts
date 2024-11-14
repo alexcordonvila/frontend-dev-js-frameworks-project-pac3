6299a5a19bf591070095b9906e705fcbd11849d4
alexcordonvila <alexcordonvila@gmail.com>
2024-11-14 19:38:37 +0100

    Traduir model.ts

---
+++ PEC3_Ej4/models/todo.model.ts
@@ -0,0 +1,21 @@
+class Todo {
+  id: string;
+  text: string;
+  complete: boolean;
+
+  constructor({ text, complete } : { text: string; complete: boolean } = { complete: false, text: '' }) {
+    this.id = this.uuidv4();
+    this.text = text;
+    this.complete = complete;
+  }
+
+  private uuidv4(): string {
+    let uuid: string = [1e7] + '' + -1e3 + -4e3 + -8e3 + -1e11;
+    return (uuid).replace(/[018]/g, (c) =>
+      (
+        Number(c) ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))
+      ).toString(16)
+    );
+  }
+}
